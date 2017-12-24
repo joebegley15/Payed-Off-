ac48b050c6ddd9fe677199c1a6bd5c56f8b88020
Joe Begley <joebegley@Joes-MacBook-Air.local>
2017-12-23 19:01:38 -0500

    Converted to funciton with flexible time. Function can now take any time period, iRate and balance as arguements.

--- Payed Off.py
+++ Payed Off.py
@@ -1,21 +1,18 @@
-# Test Case 1:
-balance = 420
-annualInterestRate = 0.2
-monthlyPaymentRate = 0.04
-max = balance
-min = 0
-perm = balance
-x = True
-while x:
-    balance = perm
-    payment = round((max + min) / 2,2)
-    print(payment,max)
-    for i in range(12):
-        balance = round((balance - payment) * (1 + (annualInterestRate) / 12),2)
-    if balance < 1 and balance > -1:
-        x = False
-    elif balance < 0:
-        max = payment
-    else:
-        min = payment
-print(payment)
+def payedOff(balance, timePeriod, iRte):
+    max = balance
+    min = 0
+    balanceReset = balance
+    payment = round((max + min) / 2, 2)
+    conditional = True
+    while conditional:
+        balance = balanceReset
+        payment = round((max + min) / 2, 2)
+        for i in range(timePeriod):
+            balance = round((balance - payment) * (1 + (iRte) / 12),2)
+        if balance > -1 and balance < 0:
+            conditional = False
+        elif balance < 0:
+            max = payment
+        else: 
+            min = payment
+    return payment
